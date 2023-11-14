679fe8ea04aa6f6b98d40726a38a317e55b0771d
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-11-14 15:07:21 +0200

    project init

---
+++ monty.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: EXIT_SUCCESS on success.
+ */
+int main(int argc, char **argv)
+{
+    return (EXIT_SUCCESS);
+}
